d4008d8558f890afbb5b69cef8e1c6b4903f0947
Anatoly Zelenin <anatoly@zelenin.de>
2026-04-09 10:29:53 +0200

    Bugfix

--- lab05_transactions/src/main/java/example/transformer/TransfersProducer.java
+++ lab05_transactions/src/main/java/example/transformer/TransfersProducer.java
@@ -18,7 +18,7 @@ public class TransfersProducer {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, BankTransferSerializer.class);
-        final String TOPIC = "bank-transactions";
+        final String TOPIC = "bank-transfers";
 
         int msgsPerSec = 1;
         if (args.length == 1) {
